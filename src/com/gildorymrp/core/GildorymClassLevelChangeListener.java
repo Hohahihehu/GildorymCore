0f13cf0959568f25f54f7e827b864eaa00a36b26
Hohahihehu <oaieuy@hotmail.com>
2013-06-17 08:57:57 -0600

    Update GildorymClassLevelChangeListener.java
    Attempting to optimize level up listener, I think that's right

--- src/com/gildorymrp/core/GildorymClassLevelChangeListener.java
+++ src/com/gildorymrp/core/GildorymClassLevelChangeListener.java
@@ -4,13 +4,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 import com.gildorymrp.api.Gildorym;
+import com.gildorymrp.api.plugin.core.Character;
 import com.gildorymrp.api.event.classes.GildorymClassLevelChangeEvent;
+import com.gildorymrp.api.plugin.classes.GildorymClassesPlugin;
 
 public class GildorymClassLevelChangeListener implements Listener {
 	
 	@EventHandler
 	public void onGildorymClassLevelChange(GildorymClassLevelChangeEvent event) {
-		event.getCharacter().setMaxHealth(Gildorym.getClassesPlugin().getHpBonus(Gildorym.getClassesPlugin().getClass(event.getCharacter())) * Gildorym.getClassesPlugin().getLevel(event.getCharacter()));
+		Character char = event.getCharacter();
+		GildorymClassesPlugin plugin = Gildorym.getClassesPlugin();
+		
+		char.setMaxHealth(plugin.getHpBonus(plugin.getClass(char)) * plugin.getLevel(char));
 	}
 
 }
